553def9689326bb578102aab3658ff37d512b265
David <davsucks@umich.edu>
2015-04-06 12:59:17 -0400

    LibraryTest#shouldDisplayFormattedTimeWhenItIsAnEmptyString passing

--- src/test/java/com/thoughtworks/library/LibraryTest.java
+++ src/test/java/com/thoughtworks/library/LibraryTest.java
@@ -112,6 +112,7 @@ public class LibraryTest {
     @Test
     public void shouldDisplayFormattedTimeWhenItIsAnEmptyString() {
 
-        // implement me
+        createLibraryAndCallWelcomeWithTime();
+        verify(dateTimeFormatter).print(time);
     }
 }
